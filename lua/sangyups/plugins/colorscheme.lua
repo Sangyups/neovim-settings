20793c4412481cbc381202286ce222a7b45d2a47
Sangyups <feels4637@gmail.com>
2023-12-08 01:45:04 +0900

    chore: apply github dark theme

--- lua/sangyups/plugins/colorscheme.lua
+++ lua/sangyups/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
   {
-    "bluz71/vim-nightfly-guicolors",
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
+      require("github-theme").setup({})
+
+      vim.cmd("colorscheme github_dark")
     end,
   },
+  -- {
+  --   "bluz71/vim-nightfly-guicolors",
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- load the colorscheme here
+  --     vim.cmd([[colorscheme nightfly]])
+  --   end,
+  -- },
   -- {
   --   "folke/tokyonight.nvim",
   --   priority = 1000, -- make sure to load this before all the other start plugins
