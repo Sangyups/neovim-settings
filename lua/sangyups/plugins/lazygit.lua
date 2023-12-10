9c15a1772e1f3ff5d2cb3ee67ce4b903b478a3e4
Sangyups <feels4637@gmail.com>
2023-12-11 01:09:40 +0900

    feat: add keymap for lazygit and select all

--- lua/sangyups/plugins/lazygit.lua
+++ lua/sangyups/plugins/lazygit.lua
@@ -4,4 +4,9 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  config = function()
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+    keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+  end,
 }
