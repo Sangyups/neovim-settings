a3bd8b81795038274fa9ef2250b0e0a85470da38
Sangyups <feels4637@gmail.com>
2023-12-16 14:33:25 +0900

    chore: add keymap for nvim-cmp

--- lua/sangyups/plugins/nvim-cmp.lua
+++ lua/sangyups/plugins/nvim-cmp.lua
@@ -31,6 +31,8 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
                 ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
