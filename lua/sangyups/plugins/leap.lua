5a6f05750905a81011fc86ca2296aff8010833bc
Sangyups <feels4637@gmail.com>
2023-12-25 21:28:13 +0900

    remove conflicting keymaps

--- lua/sangyups/plugins/leap.lua
+++ lua/sangyups/plugins/leap.lua
@@ -3,8 +3,8 @@ return {
     enabled = true,
     keys = {
         { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-        { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-        { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+        -- { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+        -- { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
     },
     config = function(_, opts)
         local leap = require("leap")
