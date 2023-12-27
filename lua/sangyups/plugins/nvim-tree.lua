816f361eeae7edcc6963a0ec2ee230699de46197
Sangyups <feels4637@gmail.com>
2023-12-27 13:06:24 +0900

    chore: group empty directories in nvim-tree

--- lua/sangyups/plugins/nvim-tree.lua
+++ lua/sangyups/plugins/nvim-tree.lua
@@ -31,6 +31,7 @@ return {
                         },
                     },
                 },
+                group_empty = true,
             },
             -- disable window_picker for
             -- explorer to work well with
