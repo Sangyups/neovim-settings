ee549989ef86a253691b98fbcccf10ac4269ae17
Sangyups <feels4637@gmail.com>
2024-01-13 17:40:15 +0900

    chore: add buffer navigation keymap

--- lua/sangyups/core/keymaps.lua
+++ lua/sangyups/core/keymaps.lua
@@ -39,3 +39,7 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 keymap.set("n", "<C-a>", "ggVG", { desc = "Select all text in current buffer" }) -- select all
+
+-- buffer navigation
+keymap.set("n", "<leader>bn", "<cmd>bnext<CR>")
+keymap.set("n", "<leader>bp", "<cmd>bprev<CR>")
