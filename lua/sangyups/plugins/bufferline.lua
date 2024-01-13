a046cc778264344c2b8eb7dcfdbeb7cd892e9c8e
Sangyups <feels4637@gmail.com>
2024-01-13 17:41:26 +0900

    chore: add keymaps for git

--- lua/sangyups/plugins/bufferline.lua
+++ lua/sangyups/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 return {
     "akinsho/bufferline.nvim",
-    event = { "BufReadPre" },
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     version = "*",
     opts = {
--- lua/sangyups/plugins/gitsigns.lua
+++ lua/sangyups/plugins/gitsigns.lua
@@ -1,5 +1,63 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    config = true,
+    config = function()
+        require("gitsigns").setup({
+            current_line_blame = true,
+            current_line_blame_opts = {
+                delay = 500,
+            },
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map("n", "]h", function()
+                    if vim.wo.diff then
+                        return "]h"
+                    end
+                    vim.schedule(function()
+                        gs.next_hunk()
+                    end)
+                    return "<Ignore>"
+                end, { expr = true })
+
+                map("n", "[h", function()
+                    if vim.wo.diff then
+                        return "[h"
+                    end
+                    vim.schedule(function()
+                        gs.prev_hunk()
+                    end)
+                    return "<Ignore>"
+                end, { expr = true })
+
+                -- Actions
+                map("n", "<leader>hs", gs.stage_hunk)
+                map("n", "<leader>hr", gs.reset_hunk)
+                map("v", "<leader>hs", function()
+                    gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end)
+                map("v", "<leader>hr", function()
+                    gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end)
+                map("n", "<leader>hu", gs.undo_stage_hunk)
+                map("n", "<leader>hp", gs.preview_hunk)
+                map("n", "<leader>hb", function()
+                    gs.blame_line({ full = true })
+                end)
+                map("n", "<leader>tb", gs.toggle_current_line_blame)
+                map("n", "<leader>hd", gs.diffthis)
+                map("n", "<leader>hD", function()
+                    gs.diffthis("~")
+                end)
+                map("n", "<leader>td", gs.toggle_deleted)
+            end,
+        })
+    end,
 }
