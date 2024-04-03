f8e2a0827decb8a89a2a329c0ce595eb467dbe02
Sangyups <feels4637@gmail.com>
2024-04-03 12:25:44 +0900

    chore: enable asynchronous formatting

--- lua/sangyups/plugins/formatting.lua
+++ lua/sangyups/plugins/formatting.lua
@@ -23,8 +23,7 @@ return {
             },
             format_on_save = {
                 lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
+                async = true,
             },
         })
 
