280307b4902ef9829406ccfc351648c30e41b1a4
Sangyups <feels4637@gmail.com>
2024-04-28 01:27:00 +0900

    chore: add yamlls support for multiply types of yaml

--- lua/sangyups/plugins/colorscheme.lua
+++ lua/sangyups/plugins/colorscheme.lua
@@ -14,19 +14,19 @@ return {
         "projekt0n/github-nvim-theme",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
-        -- config = function()
-        --     require("github-theme").setup({})
-        --     vim.cmd("colorscheme github_dark")
-        -- end,
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        lazy = false,
-        priority = 1000,
         config = function()
-            vim.cmd("colorscheme kanagawa-dragon")
+            require("github-theme").setup({})
+            vim.cmd("colorscheme github_dark")
         end,
     },
+    -- {
+    --     "rebelot/kanagawa.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         vim.cmd("colorscheme kanagawa-dragon")
+    --     end,
+    -- },
     -- {
     --   "bluz71/vim-nightfly-guicolors",
     --   priority = 1000, -- make sure to load this before all the other start plugins
