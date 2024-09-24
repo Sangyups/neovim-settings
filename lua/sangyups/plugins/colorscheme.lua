6a0ddd0bc7906a71bf3c6eca2c1d4c7fcfef4289
Sangyups <feels4637@gmail.com>
2024-09-24 10:24:18 +0900

    chore: set intellij colorscheme

--- lua/sangyups/plugins/colorscheme.lua
+++ lua/sangyups/plugins/colorscheme.lua
@@ -1,19 +1,40 @@
 return {
     {
+        "briones-gabriel/darcula-solid.nvim",
+        dependencies = { "rktjmp/lush.nvim" },
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme darcula-solid")
+
+            local lush = require("lush")
+            local darcula_solid = require("lush_theme.darcula-solid")
+
+            local spec = lush.extends({ darcula_solid }).with(function()
+                return {
+                    Visual({ bg = lush.hsl(223, 51, 33) }),
+                    String({ fg = lush.hsl(83, 27, 53), gui = "bold" }),
+                }
+            end)
+
+            lush(spec)
+        end,
+    },
+    {
         "ntk148v/habamax.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        dependencies = {
-            "rktjmp/lush.nvim",
-        },
+        -- lazy = false,
+        -- priority = 1000,
+        -- dependencies = {
+        --     "rktjmp/lush.nvim",
+        -- },
         -- config = function()
         --     vim.cmd("colorscheme habamax")
         -- end,
     },
     {
         "projekt0n/github-nvim-theme",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        -- lazy = false,
+        -- priority = 1000,
         -- config = function()
         --     local specs = {
         --         github_dark_default = {
@@ -27,17 +48,17 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-                disable_float_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-            vim.cmd("colorscheme rose-pine-moon")
-        end,
+        -- lazy = false,
+        -- priority = 1000,
+        -- config = function()
+        --     require("rose-pine").setup({
+        --         disable_background = true,
+        --         disable_float_background = true,
+        --         styles = {
+        --             italic = false,
+        --         },
+        --     })
+        --     vim.cmd("colorscheme rose-pine-moon")
+        -- end,
     },
 }
