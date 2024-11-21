586943ac2bc47a4ea714a5d1d0377c6d7d1cdd08
Sangyups <feels4637@gmail.com>
2024-11-21 12:45:26 +0900

    fix: nvim-surround not working in vscode-neovim

--- lua/sangyups/plugins/nvim-surround.lua
+++ lua/sangyups/plugins/nvim-surround.lua
@@ -1,6 +1,6 @@
 return {
     "kylechui/nvim-surround",
-    event = { "BufReadPre", "BufNewFile" },
+    event = "VeryLazy",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     config = true,
 }
