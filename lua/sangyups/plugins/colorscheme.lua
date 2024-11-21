c210494b79627d340907c4779a94eb1cd9edf8d3
Sangyups <feels4637@gmail.com>
2024-11-21 12:44:47 +0900

    revert: change theme back to darcula-solid

--- lua/sangyups/plugins/colorscheme.lua
+++ lua/sangyups/plugins/colorscheme.lua
@@ -1,24 +1,22 @@
 return {
     {
         "briones-gabriel/darcula-solid.nvim",
-        -- dependencies = { "rktjmp/lush.nvim" },
-        -- lazy = false,
-        -- priority = 1000,
-        -- config = function()
-        --     vim.cmd("colorscheme darcula-solid")
-        --
-        --     local lush = require("lush")
-        --     local darcula_solid = require("lush_theme.darcula-solid")
-        --
-        --     local spec = lush.extends({ darcula_solid }).with(function()
-        --         return {
-        --             Visual({ bg = lush.hsl(223, 51, 33) }),
-        --             String({ fg = lush.hsl(83, 27, 53), gui = "bold" }),
-        --         }
-        --     end)
-        --
-        --     lush(spec)
-        -- end,
+        dependencies = { "rktjmp/lush.nvim" },
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local lush = require("lush")
+            local darcula_solid = require("lush_theme.darcula-solid")
+
+            local spec = lush.extends({ darcula_solid }).with(function()
+                return {
+                    Visual({ bg = lush.hsl(223, 51, 33) }),
+                    String({ fg = lush.hsl(83, 27, 53), gui = "bold" }),
+                }
+            end)
+
+            lush(spec)
+        end,
     },
     {
         "ntk148v/habamax.nvim",
@@ -48,17 +46,17 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-                disable_float_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-            vim.cmd("colorscheme rose-pine-moon")
-        end,
+        -- lazy = false,
+        -- priority = 1000,
+        -- config = function()
+        --     require("rose-pine").setup({
+        --         disable_background = true,
+        --         disable_float_background = true,
+        --         styles = {
+        --             italic = false,
+        --         },
+        --     })
+        --     vim.cmd("colorscheme rose-pine-moon")
+        -- end,
     },
 }
