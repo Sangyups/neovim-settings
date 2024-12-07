8f7815bb95daa1ac36ddf1c3a884cc0e83f26036
Sangyups <feels4637@gmail.com>
2024-12-08 00:54:34 +0900

    chore: <C-c> to behave normal vim motion in dressing

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,4 +1,13 @@
 return {
     "stevearc/dressing.nvim",
     event = "VeryLazy",
+    opts = {
+        input = {
+            mappings = {
+                i = {
+                    ["<C-c>"] = false,
+                },
+            },
+        },
+    },
 }
