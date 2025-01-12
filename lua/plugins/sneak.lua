a22a54a6b287ae02fa62d6acce074183b090093a
Sangyups <feels4637@gmail.com>
2025-01-13 00:44:22 +0900

    fix: conflicting keymaps between sneak and surround

--- lua/plugins/sneak.lua
+++ lua/plugins/sneak.lua
@@ -1,8 +1,8 @@
 return {
     "justinmk/vim-sneak",
     keys = {
-        { "s", mode = { "n", "x", "o" }, desc = "Sneak Forward to" },
-        { "S", mode = { "n", "x", "o" }, desc = "Sneak Backward to" },
+        { "s", mode = { "n", "o" }, desc = "Sneak Forward to" },
+        { "S", mode = { "n", "o" }, desc = "Sneak Backward to" },
         { "gs", mode = { "n", "x", "o" }, desc = "Sneak from Windows" },
         { "dz", mode = { "n" }, desc = "Delete with sneak" },
         { "cz", mode = { "n" }, desc = "Change with sneak" },
