dac50de6b95244a796ba478840aa6239f37b93a8
Sangyups <feels4637@gmail.com>
2025-01-13 17:17:51 +0900

    chore: add render-markdown plugin

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ft = { "markdown" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        enabled = false,
+    },
+}
