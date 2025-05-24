6456931f65f4066aaefdf4072846340615a0f733
Sangyups <feels4637@gmail.com>
2025-05-25 01:20:58 +0900

    chore: disable linting when leave insert

--- lua/plugins/lsp/linting.lua
+++ lua/plugins/lsp/linting.lua
@@ -2,7 +2,7 @@ return {
     {
         "mfussenegger/nvim-lint",
         lazy = true,
-        event = { "BufReadPre", "BufNewFile", "InsertLeave" },
+        event = { "BufReadPre", "BufNewFile" },
         dependencies = { "mfussenegger/nvim-dap" },
         config = function()
             local lint = require("lint")
@@ -18,7 +18,7 @@ return {
 
             local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
                 group = lint_augroup,
                 callback = function()
                     lint.try_lint()
