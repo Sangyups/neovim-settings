8c1e8e1d97942cf86a7b6fd078792c35ccb51cbd
Sangyups <feels4637@gmail.com>
2026-03-16 16:57:07 +0900

    style(ftdetect): apply stylua formatting

--- ftdetect/dockerfile.lua
+++ ftdetect/dockerfile.lua
@@ -1,5 +1,5 @@
 vim.filetype.add({
-  pattern = {
-    ["Dockerfile.*"] = "dockerfile",
-  },
+    pattern = {
+        ["Dockerfile.*"] = "dockerfile",
+    },
 })
--- ftdetect/jenkinsfile.lua
+++ ftdetect/jenkinsfile.lua
@@ -1,5 +1,5 @@
 vim.filetype.add({
-  extension = {
-    jenkinsfile = "groovy",
-  },
+    extension = {
+        jenkinsfile = "groovy",
+    },
 })
