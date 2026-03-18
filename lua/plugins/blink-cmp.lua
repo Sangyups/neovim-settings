d569eced640dda153f44028d9953413a14765605
Sangyups <feels4637@gmail.com>
2026-03-18 13:05:15 +0900

    fix(blink-cmp): disable auto-preselect in completion list
    completion.list.selection에 preselect=false 설정을 추가하여
    자동완성 목록이 열릴 때 첫 번째 항목이 자동 선택되지 않도록 변경.
    Enter는 개행, Tab으로 첫 번째 항목 선택/삽입.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -22,6 +22,9 @@ return {
             nerd_font_variant = "mono",
         },
         completion = {
+            list = {
+                selection = { preselect = false, auto_insert = true },
+            },
             documentation = {
                 auto_show = true,
                 auto_show_delay_ms = 150,
